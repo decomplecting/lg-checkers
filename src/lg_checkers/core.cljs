194dd33037f5fef2ffd5d4528a80cee0638369d6
Milt Reder <milt@anestuary.com>
2014-11-16 13:48:34 -0500

    redo project structure
    to build release, $ lein cljsbuild once lg-checkers

--- src/lg_checkers/core.cljs
+++ src/lg_checkers/core.cljs
@@ -1,9 +1,8 @@
 (ns lg-checkers.core
   (:require [lg-checkers.board :refer [init-board]]
             [lg-checkers.ui :refer [bootstrap-ui data-ui]]
-            [figwheel.client :as fw :include-macros true]))
-
-(enable-console-print!)
+            ;[figwheel.client :as fw :include-macros true]
+            ))
 
 (defonce app-init
   (do
@@ -11,6 +10,6 @@
     (bootstrap-ui)
     (data-ui)))
 
-(fw/watch-and-reload
- ;; :websocket-url "ws://localhost:3449/figwheel-ws" default
- :jsload-callback (fn [] (print "reloaded")))
+;; (fw/watch-and-reload
+;;  ;; :websocket-url "ws://localhost:3449/figwheel-ws" default
+;;  :jsload-callback (fn [] (print "reloaded")))
